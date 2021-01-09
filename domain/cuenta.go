93670f39f967a84778bf74f32bbdb8a797cb0e3c
iyaki <ivanyaki96@gmail.com>
2021-01-09 01:34:35 -0300

    Agrego validaciones a las funcionalidades basicas de la entidad Cuenta

--- domain/cuenta.go
+++ domain/cuenta.go
@@ -1,6 +1,12 @@
 package cuenta
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+// ErrValorNoPositivo es el error devuelto al intentar operar con valores no positivos.
+var ErrValorNoPositivo = errors.New("No es posible operar con valores no positivos")
 
 // Cuenta representa una cuenta en la que depositar y retirar dinero
 type Cuenta struct {
@@ -8,21 +14,35 @@ type Cuenta struct {
 	movimientos []movimiento
 }
 
-// Depositar permite depositar una cierta cantidad de dinero en una cuenta
-func (c *Cuenta) Depositar(cantidad float64, fecha time.Time) {
+// Depositar permite depositar una cierta cantidad de dinero en una cuenta.
+// Si se intenta invocar esta funcion con un valor no positivo como primer argumento
+// recibirá el error ErrValorNoPositivo
+func (c *Cuenta) Depositar(cantidad float64, fecha time.Time) error {
+	if cantidad <= 0 {
+		return ErrValorNoPositivo
+	}
+
 	c.agregarMovimiento(cantidad, fecha)
+	return nil
 }
 
-// Retirar permite retirar una cierta cantidad de dinero de una cuenta
-func (c *Cuenta) Retirar(cantidad float64, fecha time.Time) {
+// Retirar permite retirar una cierta cantidad de dinero de una cuenta.
+// Si se intenta invocar esta funcion con un valor no positivo como primer argumento
+// recibirá el error ErrValorNoPositivo
+func (c *Cuenta) Retirar(cantidad float64, fecha time.Time) error {
+	if cantidad <= 0 {
+		return ErrValorNoPositivo
+	}
+
 	c.agregarMovimiento(-cantidad, fecha)
+	return nil
 }
 
 func (c *Cuenta) agregarMovimiento(cantidad float64, fecha time.Time) {
 	c.movimientos = append(c.movimientos, movimiento{cantidad: cantidad, fecha: fecha})
 }
 
-// Balance devuelve el balance de la cuenta basado en sus movimientos
+// Balance devuelve el balance de la cuenta basado en sus movimientos.
 func (c *Cuenta) Balance() float64 {
 	var balance float64
 	for _, movimiento := range c.movimientos {
--- domain/cuenta_test.go
+++ domain/cuenta_test.go
@@ -6,20 +6,52 @@ import (
 )
 
 func TestDepositar(t *testing.T) {
-	cuenta := Cuenta{}
+	t.Run("Camino feliz", func(t *testing.T) {
+		cuenta := Cuenta{}
 
-	cantidadADepositar := 10.0
-	hoy := time.Now()
+		cantidadADepositar := 10.0
+		hoy := time.Now()
 
-	cuenta.Depositar(cantidadADepositar, hoy)
+		err := cuenta.Depositar(cantidadADepositar, hoy)
 
-	balance := cuenta.Balance()
-	asserEquals(t, cantidadADepositar, balance)
-	
-	movimientos := cuenta.movimientos
-	asserEquals(t, 1, len(movimientos))
-	asserEquals(t, cantidadADepositar, movimientos[0].cantidad)
-	asserEquals(t, hoy, movimientos[0].fecha)
+		assertNoError(t, err)
+
+		balance := cuenta.Balance()
+		assertEquals(t, cantidadADepositar, balance)
+
+		movimientos := cuenta.movimientos
+		assertEquals(t, 1, len(movimientos))
+		assertEquals(t, cantidadADepositar, movimientos[0].cantidad)
+		assertEquals(t, hoy, movimientos[0].fecha)
+	})
+
+	t.Run("Valores negativos", func(t *testing.T) {
+		cuenta := Cuenta{}
+
+		err := cuenta.Depositar(-10.0, time.Now())
+
+		assertError(t, ErrValorNoPositivo, err)
+
+		balance := cuenta.Balance()
+		assertEquals(t, 0.0, balance)
+
+		movimientos := cuenta.movimientos
+		assertEquals(t, 0, len(movimientos))
+	})
+
+	t.Run("Valor cero", func(t *testing.T) {
+		cuenta := Cuenta{}
+
+		err := cuenta.Depositar(0, time.Now())
+
+		assertError(t, ErrValorNoPositivo, err)
+
+		balance := cuenta.Balance()
+		assertEquals(t, 0.0, balance)
+
+		movimientos := cuenta.movimientos
+		assertEquals(t, 0, len(movimientos))
+	})
 }
 
 func TestRetirar(t *testing.T) {
@@ -31,16 +63,96 @@ func TestRetirar(t *testing.T) {
 
 	balance := cuenta.Balance()
 	expected := -5.0
-	asserEquals(t, expected, balance)
-	
+	assertEquals(t, expected, balance)
+
 	movimientos := cuenta.movimientos
-	asserEquals(t, 1, len(movimientos))
-	asserEquals(t, expected, movimientos[0].cantidad)
-	asserEquals(t, hoy, movimientos[0].fecha)
+	assertEquals(t, 1, len(movimientos))
+	assertEquals(t, expected, movimientos[0].cantidad)
+	assertEquals(t, hoy, movimientos[0].fecha)
+
+	t.Run("Camino feliz", func(t *testing.T) {
+		cuenta := Cuenta{}
+
+		hoy := time.Now()
+
+		err := cuenta.Retirar(5, hoy)
+
+		assertNoError(t, err)
+
+		balance := cuenta.Balance()
+		expected := -5.0
+		assertEquals(t, expected, balance)
+
+		movimientos := cuenta.movimientos
+		assertEquals(t, 1, len(movimientos))
+		assertEquals(t, expected, movimientos[0].cantidad)
+		assertEquals(t, hoy, movimientos[0].fecha)
+	})
+
+	t.Run("Valores negativos", func(t *testing.T) {
+		cuenta := Cuenta{}
+
+		err := cuenta.Retirar(-10.0, time.Now())
+
+		assertError(t, ErrValorNoPositivo, err)
+
+		balance := cuenta.Balance()
+		assertEquals(t, 0.0, balance)
+
+		movimientos := cuenta.movimientos
+		assertEquals(t, 0, len(movimientos))
+	})
+
+	t.Run("Valor cero", func(t *testing.T) {
+		cuenta := Cuenta{}
+
+		err := cuenta.Retirar(0, time.Now())
+
+		assertError(t, ErrValorNoPositivo, err)
+
+		balance := cuenta.Balance()
+		assertEquals(t, 0.0, balance)
+
+		movimientos := cuenta.movimientos
+		assertEquals(t, 0, len(movimientos))
+	})
 }
 
-func asserEquals(t *testing.T, expected, got interface{}) {
+func TestBalance(t *testing.T) {
+	cuenta := Cuenta{}
+
+	cuenta.Depositar(5.0, time.Now())
+	cuenta.Depositar(10.25, time.Now())
+	cuenta.Depositar(100.0, time.Now())
+	cuenta.Retirar(75.5, time.Now())
+	cuenta.Depositar(5.99, time.Now())
+	cuenta.Retirar(2.0, time.Now())
+
+	assertEquals(t, 43.74, cuenta.Balance())
+}
+
+func assertEquals(t *testing.T, expected, got interface{}) {
 	t.Helper()
+
+	if expected != got {
+		t.Errorf("expected %v got %v", expected, got)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Errorf("error not expected but %v", got)
+	}
+}
+
+func assertError(t *testing.T, expected, got error) {
+	t.Helper()
+
+	if got == nil {
+		t.Errorf("an error was expected but none given")
+	}
 
 	if expected != got {
 		t.Errorf("expected %v got %v", expected, got)
